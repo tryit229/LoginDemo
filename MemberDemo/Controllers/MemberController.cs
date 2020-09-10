f70f49f0f1a24b49717661b7e50196bbdcce403e
Janet <tryit229@gmail.com>
2020-09-10 14:39:32 +0800

    structure

---
+++ MemberDemo/Controllers/MemberController.cs
@@ -0,0 +1,37 @@
+﻿using MemberDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MemberDemo.Controllers
+{
+    public class MemberController : ApiController
+    {
+        [HttpPost]
+        public Response<string> Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public Response<string> Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public Response<string> ForgetPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public Response<bool> IsExisted()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MemberDemo/Models/Response.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MemberDemo.Models
+{
+    public class Response<TData>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public TData Data { get; set; }
+
+        //public string ErrorID { get; set; }
+        //TODO 若有定義錯誤帶碼，可供前端更有效率判斷錯誤訊息，也方便多語系的置換。
+    }
+}
